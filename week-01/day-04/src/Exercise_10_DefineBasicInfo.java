68b6a14d64df955d477bacf97115906b0a9648e7
balazs-varga <balazsvarga91@gmail.com>
2018-02-15 13:45:38 +0100

    feat(Exercise_10_DefineBasicInfo): DefineBasicInfo is done

---
+++ week-01/day-04/src/Exercise_10_DefineBasicInfo.java
@@ -0,0 +1,19 @@
+public class Exercise_10_DefineBasicInfo {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        // Your age as an integer
+        // Your height in meters as a double
+        // Whether you are married or not as a boolean
+
+        String myName = "Balazs Varga";
+        int myAge = 26;
+        double myHeight = 1.93;
+        boolean isMarried = false;
+
+        System.out.println("My name is: " + myName);
+        System.out.println("I'm " + myAge + " years old");
+        System.out.println("I'm " + myHeight + " cm tall");
+        System.out.println("Am I married? " + isMarried);
+    }
+}
