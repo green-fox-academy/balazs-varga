5df316819e5ed520c062e39515c9817b410ceb55
balazs-varga <balazsvarga91@gmail.com>
2018-02-15 12:35:07 +0100

    feat(Exercise_03_HelloOthers): HelloOthers is done

---
+++ week-01/day-04/src/Exercise_03_HelloOthers.java
@@ -0,0 +1,14 @@
+public class Exercise_03_HelloOthers {
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+
+        System.out.println("Hello, Mark!");
+        System.out.println("Hello, Laci!");
+        System.out.println("Hello, Anna!");
+    }
+}
