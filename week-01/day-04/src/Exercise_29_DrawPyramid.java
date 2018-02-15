31f60bedc8c1657d0df0b27fb03ccd3c4cc36929
balazs-varga <balazsvarga91@gmail.com>
2018-02-15 20:52:34 +0100

    feat(Exercise_29_DrawPyramid): DrawPyramid is done

---
+++ week-01/day-04/src/Exercise_29_DrawPyramid.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Exercise_29_DrawPyramid {
+    // Write a program that reads a number from the standard input, then draws a
+    // pyramid like this:
+    //
+    //
+    //    *
+    //   ***
+    //  *****
+    // *******
+    //
+    // The pyramid should have as many lines as the number was
+
+    public static void main(String[] args) {
+        String symbols = "*";
+        int stars = 1;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please provide the number of the triangle's lines:");
+        int triangleLines = scanner.nextInt();
+        int spaces = triangleLines-1;
+
+        for (int i = 0; i < triangleLines; i++) {
+            for (int x = 0; x < spaces; x++){
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < stars; j++) {
+                System.out.print(symbols);
+            }
+            System.out.println();
+            stars += 2;
+            spaces--;
+        }
+    }
+}
+
