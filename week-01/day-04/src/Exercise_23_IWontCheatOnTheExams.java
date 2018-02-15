1f9798317fa4fd140acfb485efd698405a5f8e1d
balazs-varga <balazsvarga91@gmail.com>
2018-02-15 17:26:07 +0100

    feat(Exercise_23_IWontCheatOnTheExams): IWontCheatOnTheExams is done

---
+++ week-01/day-04/src/Exercise_23_IWontCheatOnTheExams.java
@@ -0,0 +1,10 @@
+public class Exercise_23_IWontCheatOnTheExams {
+    // Create a program that writes this line 100 times:
+    // "I won't cheat on the exam!"
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("I won't cheat on the exam!");
+        }
+    }
+}
