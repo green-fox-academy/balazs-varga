4e4a0e9f6d801137e0de258e362c3b5316db2f66
balazs-varga <balazsvarga91@gmail.com>
2018-02-17 20:51:00 +0100

    feat(Exercise_32_DrawDiagonal): DrawDiagonal is done

---
+++ week-01/day-04/src/Exercise_32_DrawDiagonal.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Exercise_32_DrawDiagonal {
+
+    // Write a program that reads a number from the standard input, then draws a
+    // square like this:
+    //
+    //
+    // %%%%%
+    // %%  %
+    // % % %
+    // %  %%
+    // %%%%%
+    //
+    // The square should have as many lines as the number was
+
+    public static void main(String[] args) {
+        String symbol = "%";
+        int innerSymbolPosition = 1;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please provide the number of the square's lines and press Enter:");
+        int squareLines = scanner.nextInt();
+
+        for (int i = 0; i < squareLines; i++) {
+            if (i == 0 || i == squareLines - 1) {
+                for (int j = 0; j < squareLines; j++) {
+                    System.out.print(symbol);
+                }
+                System.out.println();
+            }else if (i > 0 && i < squareLines -1) {
+                for (int k = 0; k < squareLines; k++) {
+                    if (k == 0 || k == squareLines -1) {
+                        System.out.print(symbol);
+                    }else if(k == innerSymbolPosition) {
+                        System.out.print(symbol);
+                    }else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+                innerSymbolPosition++;
+            }
+        }
+    }
+}
