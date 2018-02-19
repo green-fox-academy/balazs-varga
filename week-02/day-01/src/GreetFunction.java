232eff0140b2f5c0832cb45ffaf37fdf7d346e7e
balazs-varga <balazsvarga91@gmail.com>
2018-02-19 12:33:26 +0100

    feat(GreetFunction): task is done

---
+++ week-02/day-01/src/GreetFunction.java
@@ -0,0 +1,20 @@
+public class GreetFunction {
+
+    //  From now on, create the usual class wrapper
+    //  and main method on your own.
+
+    // - Create a string variable named `al` and assign the value `Greenfox` to it
+    // - Create a function called `greet` that greets it's input parameter
+    // - Greeting is printing e.g. `Greetings dear, Greenfox`
+    // - Greet `al`
+
+    public static void main(String[] args) {
+        String al = "Greenfox";
+
+        greet(al);
+    }
+
+    public static void greet(String greetName) {
+        System.out.println("Greetings dear, " + greetName);
+    }
+}
