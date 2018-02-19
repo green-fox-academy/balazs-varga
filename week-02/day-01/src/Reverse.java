dc2cfc62adac3e529be81c29e4d879f6b89ee4ad
balazs-varga <balazsvarga91@gmail.com>
2018-02-19 14:40:37 +0100

    feat(Reverse): created a copy from the aj array to the temp array

---
+++ week-02/day-01/src/Reverse.java
@@ -0,0 +1,16 @@
+public class Reverse {
+
+    // - Create an array variable named `aj`
+    //   with the following content: `[3, 4, 5, 6, 7]`
+    // - Reverse the order of the elements in `aj`
+    // - Print the elements of the reversed `aj`
+
+    public static void main(String[] args) {
+        int[] aj = {3, 4, 5, 6, 7};
+        int[] temp = new int[5];
+
+        for (int i = 0; i < aj.length; i++) {
+            temp[i] = aj[i];
+        }
+    }
+}
