d66e7741a4c94feea0e7ac538c9aef819b1b21e5
balazs-varga <balazsvarga91@gmail.com>
2018-02-19 14:43:44 +0100

    feat(Reverse): task is done

--- week-02/day-01/src/Reverse.java
+++ week-02/day-01/src/Reverse.java
@@ -8,9 +8,21 @@ public class Reverse {
     public static void main(String[] args) {
         int[] aj = {3, 4, 5, 6, 7};
         int[] temp = new int[5];
+        int index;
 
         for (int i = 0; i < aj.length; i++) {
             temp[i] = aj[i];
+        }
+        
+        index = aj.length;
+
+        for (int i = 0; i < aj.length; i++) {
+            aj[i] = temp[index - 1];
+            index--;
+        }
+
+        for (int i = 0; i < aj.length; i++) {
+            System.out.println(aj[i]);
         }
     }
 }
