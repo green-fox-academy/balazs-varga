de887fcd011a4e8d815aea0b1234a99979a895b6
balazs-varga <balazsvarga91@gmail.com>
2018-02-19 12:55:37 +0100

    feat(PrintArguments): task is done

---
+++ week-02/day-01/src/PrintArguments.java
@@ -0,0 +1,30 @@
+public class PrintArguments {
+
+    //  Create the usual class wrapper
+    //  and main method on your own.
+
+    // - Create a function called `printer`
+    //   which prints the input String parameters
+    // - It can have any number of parameters
+
+    // Examples
+    // printer("first")
+    // printer("first", "second")
+    // printer("first", "second", "third", "fourh")
+    // ...
+
+    public static void main(String[] args) {
+        printer("first", "second", "third", "fourth");
+    }
+
+    public static void printer(String... words) {
+        if (words.length == 0) {
+            System.out.println("No argument passed");
+            return;
+        }
+
+        for (int i = 0; i < words.length; i++) {
+            System.out.println(words[i]);
+        }
+    }
+}
