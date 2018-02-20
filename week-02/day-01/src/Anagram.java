2d370777c83d783c152bc68c4ac87b7cfde07fda
balazs-varga <balazsvarga91@gmail.com>
2018-02-20 10:42:24 +0100

    feat(Unique): unique funcion modified

---
+++ week-02/day-01/src/Anagram.java
@@ -0,0 +1,25 @@
+
+
+public class Anagram {
+
+    // Create a function named is anagram following your current language's style guide.
+    // It should take two strings and return a boolean value depending on whether its an anagram or not.
+
+    public static void main(String[] args) {
+        System.out.println(anagram("fEERaa aaa", "fwewewe wew"));
+    }
+
+    public static boolean anagram(String firstWord, String secondWord) {
+        boolean isAnagram;
+
+        if (firstWord.length() != secondWord.length()) {
+            return false;
+        }else
+
+
+
+            return true;
+        }
+
+
+    }
