83d367cf915e6f70e36a1bccb6ecee57f6064f0c
balazs-varga <balazsvarga91@gmail.com>
2018-02-22 12:44:42 +0100

    feat(Anagram): task is done

--- week-02/day-01/src/Anagram.java
+++ week-02/day-01/src/Anagram.java
@@ -1,19 +1,78 @@
+import java.util.Arrays;
+
 public class Anagram {
 
-    // Create a function named is anagram following your current language's style guide.
-    // It should take two strings and return a boolean value depending on whether its an anagram or not.
+  // Create a function named is anagram following your current language's style guide.
+  // It should take two strings and return a boolean value depending on whether its an anagram or not.
 
-    public static void main(String[] args) {
-        System.out.println(anagram("fEERaa aaa", "fwewewe wew"));
+  public static void main(String[] args) {
+
+    System.out.println(anagram("Tobia Gorrio", "Arrigo Boito"));
+  }
+
+  public static boolean anagram(String firstWord, String secondWord) {
+    System.out.println("Is the " + firstWord + " anagram of " + secondWord + "?");
+
+    if (firstWord.length() != secondWord.length()) {
+      return false;
+    } else {
+      char[] uniqueLettersOfFirstWord;
+      char[] uniqueLettersOfSecondWord;
+
+      //kisbetűssé iratás és whitespacek eltüntetése
+      firstWord = convertWordToLowercaseWithoutSpaces(firstWord);
+      secondWord = convertWordToLowercaseWithoutSpaces(secondWord);
+
+      uniqueLettersOfFirstWord = findUniqueLettersOfString(firstWord);
+      Arrays.sort(uniqueLettersOfFirstWord);
+      charArrayPrinter(uniqueLettersOfFirstWord);
+
+      uniqueLettersOfSecondWord = findUniqueLettersOfString(secondWord);
+      Arrays.sort(uniqueLettersOfSecondWord);
+      charArrayPrinter(uniqueLettersOfSecondWord);
+
+      //tömbök stringé visszaalakítása
+      String temp1 = String.valueOf(uniqueLettersOfFirstWord);
+      String temp2 = String.valueOf(uniqueLettersOfSecondWord);
+
+      if (temp1.equals(temp2)) {
+        return true;
+      } else {
+        return false;
+      }
     }
+  }
 
-    public static boolean anagram(String firstWord, String secondWord) {
-        boolean isAnagram;
+  public static char[] findUniqueLettersOfString(String word){
 
-        if (firstWord.length() != secondWord.length()) {
-            return false;
-        }else
+    char[] uniqueLettersOfWord = new char[word.length()];
+    int uniqueArrayIndex = 0;
 
-            return true;
+    for (int i = 0; i < word.length(); i++) {
+      char temp = word.charAt(i);
+      int counter = 0;
+      for (int j = 0; j < word.length(); j++) {
+        if (uniqueLettersOfWord[j] != temp) {
+          counter++;
         }
+      }
+      if (counter == word.length()) {
+        uniqueLettersOfWord[uniqueArrayIndex] = temp;
+        uniqueArrayIndex++;
+      }
     }
+    return uniqueLettersOfWord;
+  }
+
+  public static String convertWordToLowercaseWithoutSpaces(String word) {
+    String modifiedWord = word.toLowerCase().replaceAll(" ", "");
+    return modifiedWord;
+  }
+
+  public static void charArrayPrinter(char[] arrayToPrint) {
+    for (int i = 0; i < arrayToPrint.length; i++) {
+      System.out.print(arrayToPrint[i]);
+    }
+    System.out.println();
+  }
+}
