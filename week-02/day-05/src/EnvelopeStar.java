00f3cd7cb351e65e5175a137627047a3c5b696d1
balazs-varga <balazsvarga91@gmail.com>
2018-02-24 17:36:46 +0100

    refactor(EvelopeStar): created new variables

--- week-02/day-05/src/EnvelopeStar.java
+++ week-02/day-05/src/EnvelopeStar.java
@@ -12,52 +12,56 @@ public class EnvelopeStar {
 
   public static void drawFirstHalfOfFigure(Graphics graphics) {
     graphics.setColor(Color.green);
+    int lineSpace = 10;
+    int lineNumber = 16;
     int xStartPosition = 10;
     int yStartPosition = HEIGHT / 2;
     int xEndPosition = WIDTH / 2;
-    int yEndPosition = yStartPosition - 10;
+    int yEndPosition = yStartPosition - lineSpace;
     int xOffset = 0;
     int yOffset = 0;
 
-    for (int i = 0; i < 16; i++) {
+    for (int i = 0; i < lineNumber; i++) {
       graphics.drawLine(xStartPosition + xOffset, yStartPosition, xEndPosition, yEndPosition + yOffset);
-      xOffset += 10;
-      yOffset -= 10;
+      xOffset += lineSpace;
+      yOffset -= lineSpace;
     }
 
-    yEndPosition = yStartPosition + 10;
+    yEndPosition = yStartPosition + lineSpace;
     xOffset = 0;
     yOffset = 0;
 
-    for (int i = 0; i < 15; i++) {
+    for (int i = 0; i < lineNumber - 1; i++) {
       graphics.drawLine(xStartPosition + xOffset, yStartPosition, xEndPosition, yEndPosition + yOffset);
-      xOffset += 10;
-      yOffset += 10;
+      xOffset += lineSpace;
+      yOffset += lineSpace;
     }
   }
 
   public static void drawSecondHalfOfFigure(Graphics graphics) {
+    int lineNumber = 16;
+    int lineSpace = 10;
     int xStartPosition = WIDTH - 10;
     int yStartPosition = HEIGHT / 2;
     int xEndPosition = WIDTH / 2;
-    int yEndPosition = yStartPosition - 10;
+    int yEndPosition = yStartPosition - lineSpace;
     int xOffset = 0;
     int yOffset = 0;
 
-    for (int i = 0; i < 15; i++) {
+    for (int i = 0; i < lineNumber - 1; i++) {
       graphics.drawLine(xStartPosition + xOffset, yStartPosition, xEndPosition, yEndPosition + yOffset);
-      xOffset -= 10;
-      yOffset -= 10;
+      xOffset -= lineSpace;
+      yOffset -= lineSpace;
     }
 
-    yEndPosition = yStartPosition + 10;
+    yEndPosition = yStartPosition + lineSpace;
     xOffset = 0;
     yOffset = 0;
 
-    for (int i = 0; i < 16; i++) {
+    for (int i = 0; i < lineNumber; i++) {
       graphics.drawLine(xStartPosition + xOffset, yStartPosition, xEndPosition, yEndPosition + yOffset);
-      xOffset -= 10;
-      yOffset += 10;
+      xOffset -= lineSpace;
+      yOffset += lineSpace;
     }
   }
 
