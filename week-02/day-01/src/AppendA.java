e552cff8b2a0073e6a13c99cf231fe3bd5b7e556
balazs-varga <balazsvarga91@gmail.com>
2018-02-24 14:19:49 +0100

    refactor(AppendA): changed the space indentation from 4 to 2

--- week-02/day-01/src/AppendA.java
+++ week-02/day-01/src/AppendA.java
@@ -4,15 +4,15 @@ public class AppendA {
     //   with the following content: `["kuty", "macsk", "cic"]`
     // - Add all elements an `"a"` at the end
 
-    public static void main(String[] args) {
-        String[] nimals = {"kuty", "macsk", "cic"};
+  public static void main(String[] args) {
+    String[] nimals = {"kuty", "macsk", "cic"};
 
-        for (int i = 0; i < nimals.length; i++) {
-            nimals[i] += 'a';
-        }
-
-        for (int i = 0; i < nimals.length; i++) {
-            System.out.println(nimals[i]);
-        }
+    for (int i = 0; i < nimals.length; i++) {
+      nimals[i] += 'a';
     }
+
+    for (int i = 0; i < nimals.length; i++) {
+      System.out.println(nimals[i]);
+    }
+  }
 }
