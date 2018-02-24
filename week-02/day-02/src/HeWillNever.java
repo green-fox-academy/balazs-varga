b8e18dfeb6bcad2b6ef47fbb4eec4d766353541e
balazs-varga <balazsvarga91@gmail.com>
2018-02-24 14:38:48 +0100

    refactor(HeWillNever): changed the initialisation of the HashMap

--- week-02/day-02/src/HeWillNever.java
+++ week-02/day-02/src/HeWillNever.java
@@ -5,7 +5,7 @@ public class HeWillNever {
     String out = "";
     int[] notSoCrypticMessage = {1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11};
 
-    Map<Integer, String> map = new HashMap<Integer, String>();
+    HashMap<Integer, String> map = new HashMap<Integer, String>();
 
     map.put(7, "run around and desert you");
     map.put(50, "tell a lie and hurt you ");
