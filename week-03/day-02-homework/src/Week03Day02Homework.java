b21f8c36137cbd0b4d4afeacfa24444b925263b9
balazs-varga <balazsvarga91@gmail.com>
2018-02-27 20:04:24 +0100

    feat(HomeWork): task is done

---
+++ week-03/day-02-homework/src/Week03Day02Homework.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class Week03Day02Homework {
+
+  public static void main(String[] args) {
+
+   // Create 5 trees
+   // Store the data of them in variables in your program
+   // for every tree the program should store its'
+   //   - type
+   //   - leaf color
+   //   - age
+   //   - sex
+   // you can use just variables, or lists and/or maps
+
+    ArrayList<String> arborvitae = new ArrayList<>();
+    arborvitae.add("Type: northern white-cedar");
+    arborvitae.add("Leaf color: brown");
+    arborvitae.add("Age: 44");
+    arborvitae.add("Sex: female");
+
+    ArrayList<String> blackAsh = new ArrayList<>();
+    blackAsh.add("Type: Fraxinus nigra Marshall");
+    blackAsh.add("Leaf color: grey");
+    blackAsh.add("Age: 56");
+    blackAsh.add("Sex: female");
+
+    ArrayList<String> bassWood = new ArrayList<>();
+    bassWood.add("Type: Tilia americana (Ventenat) Linnaeus");
+    bassWood.add("Leaf color: green");
+    bassWood.add("Age: 19");
+    bassWood.add("Sex: male");
+
+    ArrayList<String> blackCherry = new ArrayList<>();
+    blackCherry.add("Type: Prunus serotina Ehrhart");
+    blackCherry.add("Leaf color: brown");
+    blackCherry.add("Age: 96");
+    blackCherry.add("Sex: female");
+
+    ArrayList<String> pignutHickory = new ArrayList<>();
+    pignutHickory.add("Type: northern white-cedar");
+    pignutHickory.add("Leaf color: yellow");
+    pignutHickory.add("Age: 73");
+    pignutHickory.add("Sex: female");
+  }
+}
--- week-03/day-02/src/TicTacToe.java
+++ week-03/day-02/src/TicTacToe.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,26 +13,50 @@ public class TicTacToe {
     // game result. Return 'X'/'O'/'draw' based on which player
     // has winning situation.
 
-    System.out.println(ticTacResult("win-o.txt"));
+    //System.out.println(ticTacResult("win-o.txt"));
     // should print O
 
-    System.out.println(ticTacResult("win-x.txt"));
+    //System.out.println(ticTacResult("win-x.txt"));
     // should print X
 
-    System.out.println(ticTacResult("draw.txt"));
+    //System.out.println(ticTacResult("draw.txt"));
     // should print draw
+
+    ticTacResult("win-o.txt");
   }
 
-  public static List<String> ticTacResult(String fileName) {
+  public static void ticTacResult(String fileName) {
     Path filePath = Paths.get(fileName);
-
-    List<String> lines = null;
+    int countO = 0;
+    int countX = 0;
 
     try {
-      lines = Files.readAllLines(filePath);
-    } catch (Exception e) {
-      System.out.println("Found an error");
+      List<String> lines = Files.readAllLines(filePath);
+
+      System.out.println(lines);
+
+      for (int i = 0; i < 3; i++) {
+
+        for (int j = 0; j < 3; j++) {
+          System.out.print(lines.get(i).charAt(j));
+
+
+        }
+
+      }
+
+
+
+
+
+
+
+
+
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-    return lines;
+
+
   }
 }
