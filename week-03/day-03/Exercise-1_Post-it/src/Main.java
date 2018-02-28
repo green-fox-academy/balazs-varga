9fad5ce100dfe16bacbbb260e9940aaf74b5ba41
balazs-varga <balazsvarga91@gmail.com>
2018-02-28 11:36:06 +0100

    fix(Exercise-1_Post-it): main method is added to the Main class

--- week-03/day-03/Exercise-1_Post-it/src/Main.java
+++ week-03/day-03/Exercise-1_Post-it/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
-  PostIt postItObj1 = new PostIt("orange", "Idea 1", "blue");
-  PostIt postItObj2 = new PostIt("pink", "Awesome", "black");
-  PostIt postItObj3 = new PostIt("yellow", "Superb!", "green");
+  public static void main(String[] args) {
+    PostIt postItObj1 = new PostIt("orange", "Idea 1", "blue");
+    PostIt postItObj2 = new PostIt("pink", "Awesome", "black");
+    PostIt postItObj3 = new PostIt("yellow", "Superb!", "green");
+  }
 }
