31d0381a10add696f43e5d3dfa44f1f7d350bb69
balazs-varga <balazsvarga91@gmail.com>
2018-02-28 11:43:43 +0100

    feat(Exercise-2_BlogPost): task is done

---
+++ week-03/day-03/Exercise-2_BlogPost/src/BlogPost.java
@@ -0,0 +1,14 @@
+public class BlogPost {
+
+  private String authorName;
+  private String title;
+  private String text;
+  private String duplicationDate;
+
+  public BlogPost(String authorName, String title, String text, String duplicationDate) {
+    this.authorName = authorName;
+    this.title = title;
+    this.text = text;
+    this.duplicationDate = duplicationDate;
+  }
+}
