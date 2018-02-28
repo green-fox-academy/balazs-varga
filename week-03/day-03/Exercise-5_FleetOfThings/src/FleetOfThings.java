e5e60be5b1c0d4b1545809fdf23518188d2dd557
balazs-varga <balazsvarga91@gmail.com>
2018-02-28 14:43:32 +0100

    feat(Exercise-5_FleetOfThings): task is done

---
+++ week-03/day-03/Exercise-5_FleetOfThings/src/FleetOfThings.java
@@ -0,0 +1,27 @@
+public class FleetOfThings {
+
+  public static void main(String[] args) {
+    Fleet fleet = new Fleet();
+
+    // Create a fleet of things to have this output:
+    // 1. [ ] Get milk
+    // 2. [ ] Remove the obstacles
+    // 3. [x] Stand up
+    // 4. [x] Eat lunch
+
+    Thing firstThing = new Thing("Get milk");
+    Thing secondThing = new Thing("Remove the obstacles");
+    Thing thirdThing = new Thing("Stand up");
+    Thing fourthThing = new Thing("Eat lunch");
+
+    fleet.add(firstThing);
+    fleet.add(secondThing);
+    fleet.add(thirdThing);
+    fleet.add(fourthThing);
+
+    thirdThing.complete();
+    fourthThing.complete();
+
+    System.out.println(fleet);
+  }
+}
