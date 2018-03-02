f984fdddd6e7dca6981621150aa66d3cceb38511
balazs-varga <balazsvarga91@gmail.com>
2018-03-02 08:50:43 +0100

    feat(Exercise12_RectangleDrawing): task is done

---
+++ week-03/day-04/src/Exercise12_RectangleDrawing.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.awt.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Exercise12_RectangleDrawing {
+
+  public static void mainDraw(Graphics graphics) {
+    graphics.setColor(Color.black);
+    graphics.drawRect(0, 0, HEIGHT, WIDTH);
+    rectangleDraw(graphics, 0, 0, WIDTH);
+  }
+
+  public static void rectangleDraw(Graphics graphics, int x, int y, int size) {
+    if (size < 5) {
+      return;
+    }
+    graphics.drawRect(x + size / 3, y, size / 3, size / 3);
+    graphics.drawRect(x, y + size / 3, size / 3, size / 3);
+    graphics.drawRect(x + size / 3, y + (int) (size * 2. / 3.), size / 3, size / 3);
+    graphics.drawRect(x + (int) (size * 2. / 3.), y + size / 3, size / 3, size / 3);
+    rectangleDraw(graphics, x + size / 3, y, size / 3);
+    rectangleDraw(graphics, x, y + size / 3, size / 3);
+    rectangleDraw(graphics, x + (int) (size * 2. / 3.), y + size / 3, size / 3);
+    rectangleDraw(graphics, x + size / 3, y + (int) (size * 2. / 3.), size / 3);
+  }
+
+  //    Don't touch the code below
+  static int WIDTH = 729;
+  static int HEIGHT = 729;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      this.setBackground(Color.yellow);
+      mainDraw(graphics);
+    }
+  }
+}
