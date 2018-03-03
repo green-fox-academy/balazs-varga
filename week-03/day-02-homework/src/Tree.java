8022487606e152ee692fe06191cca33734d4eb14
balazs-varga <balazsvarga91@gmail.com>
2018-03-03 19:16:48 +0100

    feat(HomeWork): created homework with classes

---
+++ week-03/day-02-homework/src/Tree.java
@@ -0,0 +1,14 @@
+public class Tree {
+
+  private String type;
+  private String leafColor;
+  private int age;
+  private String sex;
+
+  public Tree(String type, String leafColor, int age, String sex) {
+    this.type = type;
+    this.leafColor = leafColor;
+    this.age = age;
+    this.sex = sex;
+  }
+}
---
+++ week-03/day-02-homework/src/TreeMain.java
@@ -0,0 +1,10 @@
+public class TreeMain {
+
+  public static void main(String[] args) {
+    Tree arborvitae = new Tree("northern white-cedar", "brown", 44, "female");
+    Tree blackAsh = new Tree("Fraxinus nigra Marshall", "grey", 56, "female");
+    Tree bassWood = new Tree("Tilia americana (Ventenat) Linnaeus", "green", 19, "male");
+    Tree blackCherry = new Tree("Prunus serotina Ehrhart", "brown", 96, "female");
+    Tree pignutHickory = new Tree("northern white-cedar", "yellow", 73, "female");
+  }
+}
