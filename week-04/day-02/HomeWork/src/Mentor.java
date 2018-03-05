40ebd122d7cce61c8300b78573b4dd23e5d4a5c4
balazs-varga <balazsvarga91@gmail.com>
2018-03-05 16:37:09 +0100

    feat(HomeWork before inheritance): Mentor class additional field is created

--- week-04/day-02/HomeWork/src/Mentor.java
+++ week-04/day-02/HomeWork/src/Mentor.java
@@ -1,3 +1,15 @@
 public class Mentor {
-  
+
+  private String name;
+  private int age;
+  private String gender;
+  private String level;
+
+  public void introduce() {
+    System.out.println("Hi, I'm " + this.name + " , a " + this.age + " year old " + this.gender + ".");
+  }
+
+  public void getGoal() {
+    System.out.println("My goal is: Live for the moment!");
+  }
 }
