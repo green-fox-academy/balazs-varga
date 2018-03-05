d373cbfbbf20ae45c0562ac8b599c7329d0c2f15
balazs-varga <balazsvarga91@gmail.com>
2018-03-05 16:32:13 +0100

    feat(HomeWork before inheritance): Student class introduce method is modified

--- week-04/day-02/HomeWork/src/Student.java
+++ week-04/day-02/HomeWork/src/Student.java
@@ -19,7 +19,8 @@ public class Student {
   }
 
   public void introduce() {
-    System.out.println("Hi, I'm " + this.name + " , a " + this.age + " year old " + this.gender + ".");
+    System.out.println("Hi, I'm " + this.name + " , a " + this.age + " year old " + this.gender + " from "
+            + this.previousOrganization + " who skipped " + this.skippedDays + " days from the course already.");
   }
 
   public void getGoal() {
