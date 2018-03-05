e83f289ecb28ee6ec8db5fdf71a4bfed2f96b302
balazs-varga <balazsvarga91@gmail.com>
2018-03-05 16:27:36 +0100

    feat(HomeWork before inheritance): Person class getGoal method is added

--- week-04/day-02/HomeWork/src/Person.java
+++ week-04/day-02/HomeWork/src/Person.java
@@ -19,4 +19,8 @@ public class Person {
   public void introduce() {
     System.out.println("Hi, I'm " + this.name + " , a " + this.age + " year old " + this.gender + ".");
   }
+
+  public void getGoal() {
+    System.out.println("My goal is: Live for the moment!");
+  }
 }
