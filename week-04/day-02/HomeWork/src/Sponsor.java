28b95b90ffff406345d686af47a7ceff09f246e2
balazs-varga <balazsvarga91@gmail.com>
2018-03-05 16:41:44 +0100

    feat(HomeWork before inheritance): Sponsor class additional fields are created

--- week-04/day-02/HomeWork/src/Sponsor.java
+++ week-04/day-02/HomeWork/src/Sponsor.java
@@ -1,4 +1,16 @@
 public class Sponsor {
 
-  
+  private String name;
+  private int age;
+  private String gender;
+  private String company;
+  private int hiredStudents;
+
+  public void introduce() {
+    System.out.println("Hi, I'm " + this.name + " , a " + this.age + " year old " + this.gender + ".");
+  }
+
+  public void getGoal() {
+    System.out.println("My goal is: Live for the moment!");
+  }
 }
