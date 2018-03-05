32d2df667edc1c05970c16eeb1aad52fe32d26ee
balazs-varga <balazsvarga91@gmail.com>
2018-03-05 16:45:41 +0100

    feat(HomeWork before inheritance): Sponsor class constructors are created

--- week-04/day-02/HomeWork/src/Sponsor.java
+++ week-04/day-02/HomeWork/src/Sponsor.java
@@ -6,6 +6,22 @@ public class Sponsor {
   private String company;
   private int hiredStudents;
 
+  public Sponsor(String name, int age, String gender, String company) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.company = company;
+    this.hiredStudents = 0;
+  }
+
+  public Sponsor() {
+    this.name = "Jane Doe";
+    this.age = 30;
+    this.gender = "female";
+    this.company = "Google";
+    this.hiredStudents = 0;
+  }
+
   public void introduce() {
     System.out.println("Hi, I'm " + this.name + " , a " + this.age + " year old " + this.gender +
             " who represents " + this.company + " and hired " + this.hiredStudents + " students so far.");
