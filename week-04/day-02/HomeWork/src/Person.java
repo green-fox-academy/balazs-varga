1f19bfd219fb73d8278c217783311912c0c607e4
balazs-varga <balazsvarga91@gmail.com>
2018-03-05 16:23:48 +0100

    feat(HomeWork before inheritance): Person constructors are created

--- week-04/day-02/HomeWork/src/Person.java
+++ week-04/day-02/HomeWork/src/Person.java
@@ -1,4 +1,18 @@
 public class Person {
 
-  
+  private String name;
+  private int age;
+  private String gender;
+
+  public Person(String name, int age, String gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  public Person() {
+    this.name = "Jane Doe";
+    this.age = 30;
+    this.gender = "female";
+  }
 }
