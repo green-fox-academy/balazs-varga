0c705bfcdc1837cdf4539f89d481cc4de7b5012a
balazs-varga <balazsvarga91@gmail.com>
2018-03-05 16:29:34 +0100

    feat(HomeWork before inheritance): Student class additional fields are added

--- week-04/day-02/HomeWork/src/Student.java
+++ week-04/day-02/HomeWork/src/Student.java
@@ -1,4 +1,28 @@
 public class Student {
 
-  
+  private String name;
+  private int age;
+  private String gender;
+  private String previousOrganization;
+  private int skippedDays;
+
+  public Person(String name, int age, String gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  public Person() {
+    this.name = "Jane Doe";
+    this.age = 30;
+    this.gender = "female";
+  }
+
+  public void introduce() {
+    System.out.println("Hi, I'm " + this.name + " , a " + this.age + " year old " + this.gender + ".");
+  }
+
+  public void getGoal() {
+    System.out.println("My goal is: Live for the moment!");
+  }
 }
