97255d179723760f63675663f425af391cf7ba44
balazs-varga <balazsvarga91@gmail.com>
2018-03-05 16:39:48 +0100

    feat(HomeWork before inheritance): Mentor class constructors are added

--- week-04/day-02/HomeWork/src/Mentor.java
+++ week-04/day-02/HomeWork/src/Mentor.java
@@ -5,6 +5,20 @@ public class Mentor {
   private String gender;
   private String level;
 
+  public Mentor(String name, int age, String gender, String level) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.level = level;
+  }
+
+  public Mentor() {
+    this.name = "Jane Doe";
+    this.age = 30;
+    this.gender = "female";
+    this.level = "intermediate";
+  }
+
   public void introduce() {
     System.out.println("Hi, I'm " + this.name + " , a " + this.age + " year old " + this.gender + this.level + " mentor.");
   }
