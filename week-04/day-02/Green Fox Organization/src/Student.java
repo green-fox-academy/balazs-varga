59488db85bba7f34b0f61b97f516accd6e413c1a
balazs-varga <balazsvarga91@gmail.com>
2018-03-06 12:22:16 +0100

    feat(Green Fox Organization): Student class cinstructors and introduce method added

--- week-04/day-02/Green Fox Organization/src/Student.java
+++ week-04/day-02/Green Fox Organization/src/Student.java
@@ -2,4 +2,23 @@ public class Student extends Person {
 
   private String previousOrganization;
   private int skippedDays;
+
+  public Student(String name, int age, String gender, String previousOrganization) {
+    super(name, age, gender);
+    this.previousOrganization = previousOrganization;
+    this.skippedDays = 0;
+  }
+
+  public Student() {
+    this.setName("Jane Doe");
+    this.setAge(30);
+    this.setGender("female");
+    this.previousOrganization = "The School of Life";
+    this.skippedDays = 0;
+  }
+
+  public void introduce() {
+    System.out.println("Hi, I'm " + getName() + " , a " + getAge() + " year old " + getGender() + " from "
+            + previousOrganization + " who skipped " + skippedDays + " days from the course already.");
+  }
 }
