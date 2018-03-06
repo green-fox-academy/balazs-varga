eed1486edf655627b32935dc2aac031ce2441b30
balazs-varga <balazsvarga91@gmail.com>
2018-03-06 12:29:45 +0100

    feat(Green Fox Organization): Mentor class introduce and getGoal methods are created

--- week-04/day-02/Green Fox Organization/src/GreenFoxOrganization/Mentor.java
+++ week-04/day-02/Green Fox Organization/src/GreenFoxOrganization/Mentor.java
@@ -3,4 +3,12 @@ package GreenFoxOrganization;
 public class Mentor extends Person{
 
   private String level;
+
+  public void introduce() {
+    System.out.println("Hi, I'm " + getName() + " , a " + getAge() + " year old " + getGender() + level + " mentor.");
+  }
+
+  public void getGoal() {
+    System.out.println("Educate brilliant junior software developers.");
+  }
 }
