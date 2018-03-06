25147bb738bb5aa83dac555b4b7335045c2135f6
balazs-varga <balazsvarga91@gmail.com>
2018-03-06 12:41:14 +0100

    feat(Green Fox Organization): Cohort class and additional fields are added

---
+++ week-04/day-02/Green Fox Organization/src/GreenFoxOrganization/Cohort.java
@@ -0,0 +1,11 @@
+package GreenFoxOrganization;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cohort extends Person {
+
+  private String name;
+  private List<Student> students = new ArrayList<>();
+  private List<Mentor> mentors = new ArrayList<>();
+}
