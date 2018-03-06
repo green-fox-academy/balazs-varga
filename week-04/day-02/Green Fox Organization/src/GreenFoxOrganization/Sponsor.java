4ca66f68907f8772468348785edf3db00f595666
balazs-varga <balazsvarga91@gmail.com>
2018-03-06 12:38:42 +0100

    feat(Green Fox Organization): Sponsor class constructors are added

--- week-04/day-02/Green Fox Organization/src/GreenFoxOrganization/Sponsor.java
+++ week-04/day-02/Green Fox Organization/src/GreenFoxOrganization/Sponsor.java
@@ -5,6 +5,18 @@ public class Sponsor extends Person {
   private String company;
   private int hiredStudents;
 
+  public Sponsor(String name, int age, String gender, String company) {
+    super(name, age, gender);
+    this.company = company;
+    this.hiredStudents = 0;
+  }
+
+  public Sponsor() {
+    super();
+    company = "Google";
+    hiredStudents = 0;
+  }
+
   public void introduce() {
     System.out.println("Hi, I'm " + getName() + " , a " + getAge() + " year old " + getGender() +
             " who represents " + company + " and hired " + hiredStudents + " students so far.");
