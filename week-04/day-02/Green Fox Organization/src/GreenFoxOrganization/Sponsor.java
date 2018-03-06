3c90d4a7bb67fa107d6f171b523191fcd11d3fb3
balazs-varga <balazsvarga91@gmail.com>
2018-03-06 12:36:32 +0100

    feat(Green Fox Organization): Sponsor class introduce, getGoal and hire methods are created

--- week-04/day-02/Green Fox Organization/src/GreenFoxOrganization/Sponsor.java
+++ week-04/day-02/Green Fox Organization/src/GreenFoxOrganization/Sponsor.java
@@ -4,4 +4,17 @@ public class Sponsor extends Person {
 
   private String company;
   private int hiredStudents;
+
+  public void introduce() {
+    System.out.println("Hi, I'm " + getName() + " , a " + getAge() + " year old " + getGender() +
+            " who represents " + company + " and hired " + hiredStudents + " students so far.");
+  }
+
+  public void getGoal() {
+    System.out.println("Hire brilliant junior software developers.");
+  }
+
+  public int hire() {
+    return hiredStudents++;
+  }
 }
