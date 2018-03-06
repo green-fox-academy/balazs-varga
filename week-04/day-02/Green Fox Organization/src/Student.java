98bcc09cbb2858fd246c822aa8a9460980f3aeed
balazs-varga <balazsvarga91@gmail.com>
2018-03-06 12:24:15 +0100

    feat(Green Fox Organization): Student class getGoal and skipDays methods are added

--- week-04/day-02/Green Fox Organization/src/Student.java
+++ week-04/day-02/Green Fox Organization/src/Student.java
@@ -21,4 +21,12 @@ public class Student extends Person {
     System.out.println("Hi, I'm " + getName() + " , a " + getAge() + " year old " + getGender() + " from "
             + previousOrganization + " who skipped " + skippedDays + " days from the course already.");
   }
+
+  public void getGoal() {
+    System.out.println("Be a junior software developer.");
+  }
+
+  public int skipDays(int numberOfDays) {
+    return skippedDays + numberOfDays;
+  }
 }
