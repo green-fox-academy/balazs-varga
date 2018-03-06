47e1b6c5a7afe0b67d58e4fd7fe59198cf3b1ca7
balazs-varga <balazsvarga91@gmail.com>
2018-03-06 12:00:52 +0100

    feat(Green Fox Organization): Person class getGoal method is added

--- week-04/day-02/Green Fox Organization/src/Person.java
+++ week-04/day-02/Green Fox Organization/src/Person.java
@@ -7,5 +7,9 @@ public class Person {
   public void introduce() {
     System.out.println("Hi, I'm " + name + ", a " + age + " year old gender.");
   }
+
+  public void getGoal() {
+    System.out.println("My goal is: Live for the moment!");
+  }
 }
 
