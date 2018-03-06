28f270c461f3d2e9b32a8137caf144299ad04220
balazs-varga <balazsvarga91@gmail.com>
2018-03-06 12:03:04 +0100

    feat(Green Fox Organization): Person class constructors are added

--- week-04/day-02/Green Fox Organization/src/Person.java
+++ week-04/day-02/Green Fox Organization/src/Person.java
@@ -4,6 +4,18 @@ public class Person {
   private int age;
   private String gender;
 
+  public Person(String name, int age, String gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  public Person() {
+    this.name = "Jane Doe";
+    this.age = 30;
+    this.gender = "female";
+  }
+
   public void introduce() {
     System.out.println("Hi, I'm " + name + ", a " + age + " year old gender.");
   }
