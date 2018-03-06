0a074b73f93f52c369adc03fb813c93b596aa1ef
balazs-varga <balazsvarga91@gmail.com>
2018-03-06 12:44:57 +0100

    feat(Green Fox Organization): Cohort class addStudent, addMentor and info methods are added

--- week-04/day-02/Green Fox Organization/src/GreenFoxOrganization/Cohort.java
+++ week-04/day-02/Green Fox Organization/src/GreenFoxOrganization/Cohort.java
@@ -8,4 +8,17 @@ public class Cohort extends Person {
   private String name;
   private List<Student> students = new ArrayList<>();
   private List<Mentor> mentors = new ArrayList<>();
+
+  public void addStudent(Student student) {
+    students.add(student);
+  }
+
+  public void addMentor(Mentor mentor) {
+    mentors.add(mentor);
+  }
+
+  public void info() {
+    System.out.println("The " + this.name + " cohort has " + students.size() + " students and "
+            + mentors.size() + " mentors.");
+  }
 }
