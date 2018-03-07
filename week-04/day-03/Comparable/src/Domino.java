fe9b5d4890022d04bfac08a981c8e7fdb939e2fa
balazs-varga <balazsvarga91@gmail.com>
2018-03-07 13:48:35 +0100

    feat(Comparable): sort of the dominoes is done

---
+++ week-04/day-03/Comparable/src/Domino.java
@@ -0,0 +1,54 @@
+
+/*
+  Check out what is the Comparable interface
+  Get your Domino class
+  Implement the Comparable interface on the Domino
+  why? we want to compare based on the result of Domino.compareTo()
+  more info here
+  First compare the first value and then the second
+
+        List<Domino> dominoes = new ArrayList<>();
+        dominoes.add(new Domino(5, 2));
+        dominoes.add(new Domino(4, 6));
+        dominoes.add(new Domino(1, 5));
+        dominoes.add(new Domino(6, 7));
+        dominoes.add(new Domino(2, 4));
+        dominoes.add(new Domino(7, 1));
+
+        Collections.sort(dominoes);
+
+  the order of your dominoes should look like this: [[1,5], [2,4], [4,6], [5,2], [6,7], [7,1]]
+  Make your Todo class implement Comparable
+  It should compare the completed field first
+  Then the description
+  Or if you haven't got a separate Todo class, or it doesn't contain fields like description and completed, use your Thing class
+*/
+
+public class Domino implements Comparable<Domino> {
+
+  private final int[] values;
+
+  Domino(int valueA, int valueB) {
+    this.values = new int[]{valueA, valueB};
+  }
+
+  public int[] getValues() {
+    return values;
+  }
+
+  @Override
+  public String toString() {
+    return "[" + values[0] + ", " + values[1] + "]";
+  }
+
+  @Override
+  public int compareTo(Domino compareDomino) {
+    int firstNumber = compareDomino.getValues()[0];
+    int secondNumber = compareDomino.getValues()[1];
+    if (this.getValues()[0] != firstNumber) {
+      return this.getValues()[0] - firstNumber;
+    } else {
+      return this.getValues()[1] - secondNumber;
+    }
+  }
+}
