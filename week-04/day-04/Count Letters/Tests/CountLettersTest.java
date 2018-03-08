c89c2d0bcec53cececb1d537092e8b218f128af9
balazs-varga <balazsvarga91@gmail.com>
2018-03-08 12:42:14 +0100

    feat(CountLetters): CountLetters class and method is created

---
+++ week-04/day-04/Count Letters/Tests/CountLettersTest.java
@@ -0,0 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CountLettersTest {
+
+  CountLetters string;
+
+  @Before
+  public void init() {
+    string = new CountLetters("aasffgrgha");
+  }
+
+  @Test
+  public void letterOccurrenceCountTest() {
+  }
+}
---
+++ week-04/day-04/Count Letters/src/CountLetters.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+
+public class CountLetters {
+
+  // Write a function, that takes a string as an argument and returns a dictionary with all letters in the string
+  // as keys, and numbers as values that shows how many occurrences there are.
+
+  // Create a test for that.
+
+  private String string;
+
+  public CountLetters(String string) {
+    this.string = string;
+  }
+
+  public HashMap<Character, Integer> letterOccurrenceCount(String inputString) {
+    HashMap<Character, Integer> mapOfLetters = new HashMap<Character, Integer>();
+    String stringWithoudSpaces = inputString.replaceAll(" ", "");
+    for (int i = 0; i < stringWithoudSpaces.length(); i++) {
+      char tempChar = stringWithoudSpaces.charAt(i);
+      Integer occurance = mapOfLetters.get(tempChar);
+      if (occurance != null) {
+        mapOfLetters.put(tempChar, occurance + 1);
+      } else {
+        mapOfLetters.put(tempChar, 1);
+      }
+    }
+    return mapOfLetters;
+  }
+
+  public String getString() {
+    return string;
+  }
+}
