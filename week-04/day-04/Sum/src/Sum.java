9a0008d34b678e670e9db88baef22bba59bd5e53
balazs-varga <balazsvarga91@gmail.com>
2018-03-08 11:02:22 +0100

    feat(Sum): Sum class and sum method is created

---
+++ week-04/day-04/Sum/src/Sum.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sum {
+
+  //Create a sum method in your class which has an ArrayList of Integers as parameter
+  //It should return the sum of the elements in the list
+  //Follow these steps:
+  //   Add a new test case
+  //   Instantiate your class
+  //   create a list of integers
+  //   use the assertEquals to test the result of the created sum method
+  //Run them
+  //Create different tests where you
+  //   test your method with an empyt list
+  //   with a list with one element in it
+  //   with multiple elements in it
+  //   with a null
+  //Run them
+  //Fix your code if needed
+
+  public List<Integer> numbers;
+
+  public Sum(List<Integer> numbers) {
+    this.numbers = new ArrayList<>();
+  }
+
+  public int sumOfNumbers(List<Integer> numbers) {
+    int sum = 0;
+
+    for (int number : numbers) {
+      sum += number;
+    }
+    return sum;
+  }
+}
