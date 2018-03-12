90c01f601d3304cdb291a467b83073812efff8d7
balazs-varga <balazsvarga91@gmail.com>
2018-03-12 16:53:35 +0100

    feat(Aircraft): Carrier class and constructor created

---
+++ week-04/day-02/Aircraft Carrier/src/AircraftCarrier/Carrier.java
@@ -0,0 +1,17 @@
+package AircraftCarrier;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Carrier {
+
+  private List<Aircraft> carrier;
+  private int ammoStorage;
+  private int health;
+
+  public Carrier(int ammoStorage, int health) {
+    this.ammoStorage = ammoStorage;
+    this.health = health;
+    carrier = new ArrayList<>();
+  }
+}
