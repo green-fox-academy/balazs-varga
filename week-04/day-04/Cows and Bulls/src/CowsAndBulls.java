82c9b5f453584a46c8d9f5bb9afec0d33171f074
balazs-varga <balazsvarga91@gmail.com>
2018-03-17 22:24:35 +0100

    feat(CowsAndBullsTest): the methods of the game are implemented

--- week-04/day-04/Cows and Bulls/src/CowsAndBulls.java
+++ week-04/day-04/Cows and Bulls/src/CowsAndBulls.java
@@ -10,7 +10,7 @@ public class CowsAndBulls {
   private boolean isPlaying;
   private boolean isFinished;
   private int guessNumber;
-  private final int ROUNDS = 10;
+  private int rounds;
   private Random random;
   private Scanner scanner;
 
@@ -19,19 +19,82 @@ public class CowsAndBulls {
     bulls = 0;
     random = new Random();
     scanner = new Scanner(System.in);
-    numberToGuess = random.nextInt(10000);
+    numberToGuess = random.nextInt(9000) + 1000;
     isPlaying = false;
     isFinished = false;
     guessNumber = 0;
+    rounds = 10;
   }
 
   public void gameInit() {
     System.out.println("~~~~ Welcome to the Cows And Bulls game ~~~~");
-    System.out.println("Please enter your name: ");
+    System.out.print("\nPlease enter your name: ");
     playerName = scanner.nextLine();
-    System.out.println("Hello " + playerName + "!");
-    System.out.println("Please guess the 4 digit number, you have " + ROUNDS + " attempts to do it. Have fun :)");
+    System.out.println("\nHello " + playerName + "!");
+    System.out.println("Please give a 4 digit number and press ENTER, you have " + rounds + " attempts to guess it. Have fun :)");
     isPlaying = true;
+  }
+
+  public void game() {
+    while (rounds != 0) {
+      cows = 0;
+      bulls = 0;
+
+      System.out.println(guess());
+
+      if (isPlayerWon(cows)) {
+        break;
+      } else {
+        rounds--;
+      }
+
+      isGameOver(rounds);
+    }
+  }
+
+  public boolean isPlayerWon(int cows) {
+    if (cows == 4) {
+      System.out.println("\n~~~~ YOU WON THE GAME ~~~~");
+      isPlaying = false;
+      isFinished = true;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public boolean isGameOver(int rounds) {
+    if (rounds == 0) {
+      isPlaying = false;
+      System.out.println("\n~~~~ GAME OVER ~~~~");
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public String guess() {
+    int playerGuess = scanner.nextInt();
+    String numberToGuessString = "" + numberToGuess;
+    String playerGuessString = "";
+    guessNumber++;
+
+    while (playerGuess > 10000 || playerGuess < 1000) {
+      System.out.println("Please give a 4 digit number.");
+      playerGuess = scanner.nextInt();
+    }
+
+    playerGuessString = "" + playerGuess;
+
+    for (int i = 0; i < 4; i++) {
+      if (playerGuessString.charAt(i) == numberToGuessString.charAt(i)) {
+        cows++;
+      } else if (numberToGuessString.contains("" + playerGuessString.charAt(i))) {
+        bulls++;
+      }
+    }
+
+    return "Cows: " + cows + "   Bulls: " + bulls + "   Your number: " + playerGuess + " - You left " + (rounds - 1) + " tries.";
   }
 
   public int getCows() {
@@ -49,5 +112,6 @@ public class CowsAndBulls {
   public static void main(String[] args) {
     CowsAndBulls CAB = new CowsAndBulls();
     CAB.gameInit();
+    CAB.game();
   }
 }
