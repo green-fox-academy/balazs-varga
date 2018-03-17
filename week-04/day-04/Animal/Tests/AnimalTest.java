07dbb8142bf067e6d5b0e4c9f0f5ce9739d45a93
balazs-varga <balazsvarga91@gmail.com>
2018-03-17 18:59:16 +0100

    feat(AnimalTest): animal object for testing purpose is created

---
+++ week-04/day-04/Animal/Tests/AnimalTest.java
@@ -0,0 +1,13 @@
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class AnimalTest {
+
+  Animal animal;
+
+  @Before
+  public void setUp() {
+    animal = new Animal();
+  }
+}
