f57b9d42deae0921f61cc5fca2dc69e4c083f95d
balazs-varga <balazsvarga91@gmail.com>
2018-03-26 17:19:52 +0200

    feat(Exercise_08): solution is added using for loop

--- week-06/day-01/src/Stream/Exercise_08.java
+++ week-06/day-01/src/Stream/Exercise_08.java
@@ -2,6 +2,7 @@ package Stream;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.stream.Collectors;
 
 public class Exercise_08 {
@@ -12,8 +13,29 @@ public class Exercise_08 {
 
     ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2));
 
+    HashMap<Integer, Integer> frequencyOfNumbers = numberOccurrenceCount(numbers);
+    System.out.println(frequencyOfNumbers);
+
     frequencyOfNumbers(numbers);
   }
+
+  //Solution with loops
+
+  public static HashMap<Integer, Integer> numberOccurrenceCount(ArrayList<Integer> numbers) {
+    HashMap<Integer, Integer> mapOfNumbers = new HashMap<Integer, Integer>();
+    for (int i = 0; i < numbers.size(); i++) {
+      int tempInt = numbers.get(i);
+      Integer occurrence = mapOfNumbers.get(tempInt);
+      if (occurrence != null) {
+        mapOfNumbers.put(tempInt, occurrence + 1);
+      } else {
+        mapOfNumbers.put(tempInt, 1);
+      }
+    }
+    return mapOfNumbers;
+  }
+
+  //Solution with stream
 
   public static void frequencyOfNumbers(ArrayList<Integer> numbers) {
     System.out.println(numbers.stream()
