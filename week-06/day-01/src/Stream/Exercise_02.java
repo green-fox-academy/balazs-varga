9aaf0077a322fafb23559c6cd6e2e2938fc07cdb
balazs-varga <balazsvarga91@gmail.com>
2018-03-26 11:40:18 +0200

    refact(Exercise_02): changed the style of the stream

--- week-06/day-01/src/Stream/Exercise_02.java
+++ week-06/day-01/src/Stream/Exercise_02.java
@@ -12,7 +12,10 @@ public class Exercise_02 {
 
     ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
 
-    IntSummaryStatistics stats = numbers.stream().filter(n -> n % 2 == 1).mapToInt(n -> n).summaryStatistics();
+    IntSummaryStatistics stats = numbers.stream()
+            .filter(n -> n % 2 == 1)
+            .mapToInt(n -> n)
+            .summaryStatistics();
 
     double averageOfOddNumbers = stats.getAverage();
 
