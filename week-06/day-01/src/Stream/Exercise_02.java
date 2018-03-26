a7344c171334195990f1b01719be0b2edb69bc98
balazs-varga <balazsvarga91@gmail.com>
2018-03-26 16:42:00 +0200

    feat(Exercise_02): solution is added using for loop

--- week-06/day-01/src/Stream/Exercise_02.java
+++ week-06/day-01/src/Stream/Exercise_02.java
@@ -3,6 +3,7 @@ package Stream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.IntSummaryStatistics;
+import java.util.List;
 
 public class Exercise_02 {
 
@@ -12,6 +13,25 @@ public class Exercise_02 {
 
     ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
 
+    //Solution with loops
+
+    List<Integer> oddNumbers = new ArrayList<>();
+    double sum = 0;
+
+    for (int i = 0; i < numbers.size(); i++) {
+      if (numbers.get(i) % 2 == 1) {
+        oddNumbers.add(numbers.get(i));
+      }
+    }
+
+    for (int i = 0; i < oddNumbers.size(); i++) {
+      sum += oddNumbers.get(i);
+    }
+
+    System.out.println("The average of the odd numbers in the numbers list is " + sum / oddNumbers.size());
+
+    //Solution with stream
+
     IntSummaryStatistics stats = numbers.stream()
             .filter(n -> n % 2 == 1)
             .mapToInt(n -> n)
@@ -19,6 +39,6 @@ public class Exercise_02 {
 
     double averageOfOddNumbers = stats.getAverage();
 
-    System.out.println("The average of the odd numbers of the numbers list is " + averageOfOddNumbers);
+    System.out.println("The average of the odd numbers in the numbers list is " + averageOfOddNumbers);
   }
 }
