b045a7e177a7511561aab1998c2788c52c0a2229
balazs-varga <balazsvarga91@gmail.com>
2018-03-26 16:36:31 +0200

    feat(Exercise_01): solution is added using for loop

--- week-06/day-01/src/Stream/Exercise_01.java
+++ week-06/day-01/src/Stream/Exercise_01.java
@@ -12,9 +12,24 @@ public class Exercise_01 {
     //Write a Stream Expression to get the even numbers from the following array:
 
     ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+
+    //Solution with loops
+
+    for (int i = 0; i < numbers.size(); i++) {
+      if (numbers.get(i) % 2 == 0) {
+        System.out.println(numbers.get(i));
+      }
+    }
+
+    System.out.println();
+
+    //Solution with stream
+
     numbers.stream()
             .filter(n -> n % 2 == 0)
             .forEach(System.out::println);
+
+    System.out.println();
 
     List<Integer> evenNumbers = numbers
             .stream()
