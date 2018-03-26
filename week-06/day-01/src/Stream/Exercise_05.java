0c478090a162b5d89fce8b7f7cdbca274be138e4
balazs-varga <balazsvarga91@gmail.com>
2018-03-26 16:59:07 +0200

    feat(Exercise_05): solution is added using for loop

--- week-06/day-01/src/Stream/Exercise_05.java
+++ week-06/day-01/src/Stream/Exercise_05.java
@@ -6,10 +6,24 @@ public class Exercise_05 {
 
     //Write a Stream Expression to find the uppercase characters in a string!
 
-    findUppercaseCharacters("HellO WorlD, What's UP?");
+    findUppercaseCharactersLoop("HellO WorlD, What's UP?");
+    System.out.println();
+    findUppercaseCharactersStream("HellO WorlD, What's UP?");
   }
 
-  public static void findUppercaseCharacters(String string) {
+  //Solution with loops
+
+  public static void findUppercaseCharactersLoop(String string) {
+    for (int i = 0; i < string.length(); i++) {
+      if (Character.isUpperCase(string.charAt(i))) {
+        System.out.println(string.charAt(i));
+      }
+    }
+  }
+
+  //Solution with stream
+
+  public static void findUppercaseCharactersStream(String string) {
     string.chars()
             .filter(Character::isUpperCase)
             .mapToObj(c -> (char) c)
