6c6a5275041c5d275fe8080768d4de32b27bbc5c
balazs-varga <balazsvarga91@gmail.com>
2018-03-26 11:40:40 +0200

    refact(Exercise_03): changed the style of the stream

--- week-06/day-01/src/Stream/Exercise_03.java
+++ week-06/day-01/src/Stream/Exercise_03.java
@@ -12,8 +12,8 @@ public class Exercise_03 {
     ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
 
     numbers.stream()
-      .filter(n -> n > 0)
-      .map(n -> n * n)
-      .forEach(System.out::println);
+            .filter(n -> n > 0)
+            .map(n -> n * n)
+            .forEach(System.out::println);
   }
 }
