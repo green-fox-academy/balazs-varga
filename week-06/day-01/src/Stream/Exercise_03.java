3e6fd78a7a4b1beb17a07964decba109d73f43e3
balazs-varga <balazsvarga91@gmail.com>
2018-03-26 16:46:22 +0200

    feat(Exercise_03): solution is added using for loop

--- week-06/day-01/src/Stream/Exercise_03.java
+++ week-06/day-01/src/Stream/Exercise_03.java
@@ -12,15 +12,30 @@ public class Exercise_03 {
     //Write a Stream Expression to get the squared value of the positive numbers from the following array:
 
     ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+
+    //Solution with loops
+
+    List<Integer> squaredPositiveNumbersLoop = new ArrayList<>();
+
+    for (int i = 0; i < numbers.size(); i++) {
+      if (numbers.get(i) > 0) {
+        squaredPositiveNumbersLoop.add(numbers.get(i) * numbers.get(i));
+      }
+    }
+
+    System.out.println(squaredPositiveNumbersLoop);
+
+    //Solution with stream
+
     numbers.stream()
             .filter(n -> n > 0)
             .map(n -> n * n)
             .forEach(System.out::println);
 
-    List<Integer> squaredPositiveNumbers = numbers.stream()
+    List<Integer> squaredPositiveNumbersStream = numbers.stream()
             .filter(n -> n > 0)
             .map(n -> n * n)
             .collect(Collectors.toList());
-    System.out.println(squaredPositiveNumbers);
+    System.out.println(squaredPositiveNumbersStream);
   }
 }
