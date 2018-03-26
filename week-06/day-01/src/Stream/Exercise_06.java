b825d75a1dfaf567f0345a4116af41337959c0ef
balazs-varga <balazsvarga91@gmail.com>
2018-03-26 11:57:32 +0200

    feat(Exercise_06): task is done

---
+++ week-06/day-01/src/Stream/Exercise_06.java
@@ -0,0 +1,19 @@
+package Stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Exercise_06 {
+
+  public static void main(String[] args) {
+
+    //Write a Stream Expression to find the strings which starts with A and ends with I in the following array:
+
+    ArrayList<String> cities = new ArrayList<>(Arrays.asList("ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH",
+            "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS"));
+
+    cities.stream()
+            .filter(s -> s.charAt(0) == 'A' && s.charAt(s.length() - 1) == 'I')
+            .forEach(System.out::println);
+  }
+}
