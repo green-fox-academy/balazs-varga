a40dc127c346a0bb6bcb807e9a4227993ca251ad
balazs-varga <balazsvarga91@gmail.com>
2018-03-26 17:01:25 +0200

    feat(Exercise_06): solution is added using for loop

--- week-06/day-01/src/Stream/Exercise_06.java
+++ week-06/day-01/src/Stream/Exercise_06.java
@@ -12,6 +12,16 @@ public class Exercise_06 {
     ArrayList<String> cities = new ArrayList<>(Arrays.asList("ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH",
             "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS"));
 
+    //Solution with loops
+
+    for (int i = 0; i < cities.size(); i++) {
+      if (cities.get(i).charAt(0) == 'A' && cities.get(i).charAt(cities.get(i).length() - 1) == 'I') {
+        System.out.println(cities.get(i));
+      }
+    }
+    
+    //Solution with stream
+
     cities.stream()
             .filter(s -> s.charAt(0) == 'A' && s.charAt(s.length() - 1) == 'I')
             .forEach(System.out::println);
