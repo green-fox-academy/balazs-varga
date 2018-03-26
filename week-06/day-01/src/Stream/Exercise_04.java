d86693fe53ecbd10748b06e62fc9d9efd7e8702f
balazs-varga <balazsvarga91@gmail.com>
2018-03-26 16:53:17 +0200

    feat(Exercise_04): solution is added using for loop

--- week-06/day-01/src/Stream/Exercise_04.java
+++ week-06/day-01/src/Stream/Exercise_04.java
@@ -13,16 +13,28 @@ public class Exercise_04 {
 
     ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(3, 9, 2, 8, 6, 5));
 
+    //Solution with loops
+
+    List<Integer> resultLoop = new ArrayList<>();
+
+    for (int i = 0; i < numbers.size(); i++) {
+      if ((numbers.get(i) * numbers.get(i)) > 20) {
+        resultLoop.add(numbers.get(i));
+      }
+    }
+
+    System.out.println(resultLoop);
+
+    //Solution with stream
+
     numbers.stream()
-            .map(n -> n * n)
-            .filter(n -> n > 20)
+            .filter(n -> n * n > 20)
             .forEach(System.out::println);
 
-    List<Integer> result = numbers.stream()
-            .map(n -> n * n)
-            .filter(n -> n > 20)
+    List<Integer> resultStream = numbers.stream()
+            .filter(n -> n * n > 20)
             .collect(Collectors.toList());
 
-    System.out.println(result);
+    System.out.println(resultStream);
   }
 }
