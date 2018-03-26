97c1c961d14a77ce07025ddd480c69b25f4b13b8
balazs-varga <balazsvarga91@gmail.com>
2018-03-26 12:42:34 +0200

    feat(Exercise_5): task is done

--- week-06/day-01/src/Stream/Exercise_05.java
+++ week-06/day-01/src/Stream/Exercise_05.java
@@ -1,20 +1,18 @@
 package Stream;
 
-import java.util.stream.Stream;
-
 public class Exercise_05 {
 
   public static void main(String[] args) {
 
     //Write a Stream Expression to find the uppercase characters in a string!
 
-    findUppercaseCharacters("Hello World, What's Up?");
+    findUppercaseCharacters("HellO WorlD, What's UP?");
   }
 
   public static void findUppercaseCharacters(String word) {
-    Stream.of(word.split(" "))
-            .filter(s -> s.charAt(0) >= 65 && s.charAt(0) <= 90)
-            .map(s -> s.charAt(0))
+    word.chars()
+            .filter(Character::isUpperCase)
+            .mapToObj(c -> (char) c)
             .forEach(System.out::println);
   }
 }
