16c9fddfd7282342a6ccab928a13f4fad93c4fe2
balazs-varga <balazsvarga91@gmail.com>
2018-04-03 15:31:47 +0200

    feat(Spring Start): Say Hello to all the world webapp list the hello words

--- week-07/day-02/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
+++ week-07/day-02/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
@@ -1,12 +1,22 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Greeting {
   private long id;
   private String content;
+  private String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+          "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+          "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+          "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
 
   public Greeting(long id, String content) {
     this.id = id;
     this.content = content;
+  }
+
+  public Greeting() {
   }
 
   public long getId() {
@@ -16,4 +26,9 @@ public class Greeting {
   public String getContent() {
     return content;
   }
+
+  public String[] getHellos() {
+    return hellos;
+  }
+
 }
--- week-07/day-02/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloLanguagesWebController.java
+++ week-07/day-02/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloLanguagesWebController.java
@@ -1,4 +1,19 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Random;
+
+@Controller
 public class HelloLanguagesWebController {
+
+  Greeting greeting = new Greeting();
+  Random random = new Random();
+
+  @RequestMapping("/web/hello")
+  public String hellos(Model model, String hello) {
+    model.addAttribute("hello", greeting.getHellos());
+    return "greetinglanguages";
+  }
 }
