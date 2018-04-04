36eb94c0fa128c7c49061b41283f08ea63e778dd
balazs-varga <balazsvarga91@gmail.com>
2018-04-04 11:39:32 +0200

    feat(BankOfSimba): created bankaccointfields.html which displazs a bank account fields

--- week-07/day-03/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/BankAccountWebController.java
+++ week-07/day-03/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/BankAccountWebController.java
@@ -1,4 +1,18 @@
 package com.greenfoxacademy.bankofsimba.controller;
 
+import com.greenfoxacademy.bankofsimba.model.BankAccount;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BankAccountWebController {
+
+  BankAccount bankAccount;
+
+  @GetMapping(value = "/show")
+  public String showBankAccount(Model model) {
+    model.addAttribute("bankAccount", new BankAccount("Simba", "2000", "lion"));
+    return "bankaccountfields";
+  }
 }
--- week-07/day-03/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/model/BankAccount.java
+++ week-07/day-03/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/model/BankAccount.java
@@ -2,6 +2,21 @@ package com.greenfoxacademy.bankofsimba.model;
 
 public class BankAccount {
   private String name;
-  private int balance;
+  private String balance;
   private String animalType;
+
+  public BankAccount(String name, String balance, String animalType) {
+    this.name = name;
+    this.balance = balance;
+    this.animalType = animalType;
+  }
+
+  @Override
+  public String toString() {
+    return "BankAccount{" +
+            "name='" + name + '\'' +
+            ", balance='" + balance + '\'' +
+            ", animalType='" + animalType + '\'' +
+            '}';
+  }
 }
