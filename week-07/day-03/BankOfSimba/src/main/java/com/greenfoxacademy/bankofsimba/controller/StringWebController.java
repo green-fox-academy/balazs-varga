9649dc6ed220c9cab967b8265af22f683108cbdf
balazs-varga <balazsvarga91@gmail.com>
2018-04-04 12:04:10 +0200

    feat(BankOfSimba): HTMLception task is done

--- week-07/day-03/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/StringWebController.java
+++ week-07/day-03/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/StringWebController.java
@@ -1,4 +1,15 @@
 package com.greenfoxacademy.bankofsimba.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class StringWebController {
+
+  @GetMapping(value="/enjoy")
+  public String printString(Model model) {
+    model.addAttribute("string", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
+    return "enjoy";
+  }
 }
