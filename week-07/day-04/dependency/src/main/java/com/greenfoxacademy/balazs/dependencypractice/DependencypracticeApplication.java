bdfc1c7393bf4d4caf1c06e416ce5db258e5074d
balazs-varga <balazsvarga91@gmail.com>
2018-04-05 11:26:47 +0200

    feat(Dependency): created model and controller packages

---
+++ week-07/day-04/dependency/src/main/java/com/greenfoxacademy/balazs/dependencypractice/DependencypracticeApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.balazs.dependencypractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencypracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencypracticeApplication.class, args);
+	}
+}
