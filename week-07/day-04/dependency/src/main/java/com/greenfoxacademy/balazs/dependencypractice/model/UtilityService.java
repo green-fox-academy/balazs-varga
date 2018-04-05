c108de1f6858651a2c18c2482a75be1fbe4169bc
balazs-varga <balazsvarga91@gmail.com>
2018-04-05 12:19:55 +0200

    feat(Dependency): UtilityService class is created

---
+++ week-07/day-04/dependency/src/main/java/com/greenfoxacademy/balazs/dependencypractice/model/UtilityService.java
@@ -0,0 +1,23 @@
+package com.greenfoxacademy.balazs.dependencypractice.model;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class UtilityService {
+  ArrayList<String> colors;
+  Random random;
+
+  public UtilityService() {
+    colors = new ArrayList<>();
+    colors.add("red");
+    colors.add("blue");
+    colors.add("lime");
+    colors.add("orange");
+    colors.add("magenta");
+    random = new Random();
+  }
+
+  public String randomColor() {
+    return colors.get(random.nextInt(colors.size()));
+  }
+}
