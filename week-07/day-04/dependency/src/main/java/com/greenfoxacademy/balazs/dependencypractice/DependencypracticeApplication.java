69b647bf15b526f0384271a7489ed242fe70b8df
balazs-varga <balazsvarga91@gmail.com>
2018-04-05 11:30:33 +0200

    feat(Dependency): implemented CommandLineRunner

--- week-07/day-04/dependency/src/main/java/com/greenfoxacademy/balazs/dependencypractice/DependencypracticeApplication.java
+++ week-07/day-04/dependency/src/main/java/com/greenfoxacademy/balazs/dependencypractice/DependencypracticeApplication.java
@@ -1,12 +1,18 @@
 package com.greenfoxacademy.balazs.dependencypractice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DependencypracticeApplication {
+public class DependencypracticeApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DependencypracticeApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
 	}
 }
