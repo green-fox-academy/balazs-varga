d102e493fe636123d7f4db764da7e267df039e05
balazs-varga <balazsvarga91@gmail.com>
2018-04-05 17:36:14 +0200

    feat(Dependency): add new student method works

--- week-07/day-04/dependency/src/main/java/com/greenfoxacademy/balazs/dependencypractice/controllers/GreenFoxWebController.java
+++ week-07/day-04/dependency/src/main/java/com/greenfoxacademy/balazs/dependencypractice/controllers/GreenFoxWebController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GreenFoxWebController {
@@ -30,5 +32,11 @@ public class GreenFoxWebController {
   @GetMapping(value = "/gfa/add")
   public String addNewStudent() {
     return "gfa_add_new_student";
+  }
+
+  @PostMapping("/gfa/save")
+  public String save(@ModelAttribute(name = "studentName") String studentName) {
+    studentService.save(studentName);
+    return "redirect:/gfa/list";
   }
 }
