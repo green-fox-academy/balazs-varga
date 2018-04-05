1220af7b6379f6c8c2fff3ebe6b5275e55114867
balazs-varga <balazsvarga91@gmail.com>
2018-04-05 12:29:56 +0200

    feat(Dependency): created lintToUtilities class

--- week-07/day-04/dependency/src/main/java/com/greenfoxacademy/balazs/dependencypractice/controller/UsefulWebController.java
+++ week-07/day-04/dependency/src/main/java/com/greenfoxacademy/balazs/dependencypractice/controller/UsefulWebController.java
@@ -1,7 +1,13 @@
 package com.greenfoxacademy.balazs.dependencypractice.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UsefulWebController {
+
+  @GetMapping(value = "/useful")
+  public String linkToUtilities() {
+    return "links_to_utilities";
+  }
 }
