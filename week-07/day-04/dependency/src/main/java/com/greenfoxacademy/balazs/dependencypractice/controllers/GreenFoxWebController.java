c287af3f898827e64072bc493d1b81f7665c422e
balazs-varga <balazsvarga91@gmail.com>
2018-04-05 17:00:55 +0200

    feat(Dependency): created listStudents method

--- week-07/day-04/dependency/src/main/java/com/greenfoxacademy/balazs/dependencypractice/controllers/GreenFoxWebController.java
+++ week-07/day-04/dependency/src/main/java/com/greenfoxacademy/balazs/dependencypractice/controllers/GreenFoxWebController.java
@@ -3,6 +3,7 @@ package com.greenfoxacademy.balazs.dependencypractice.controllers;
 import com.greenfoxacademy.balazs.dependencypractice.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -18,5 +19,11 @@ public class GreenFoxWebController {
   @GetMapping(value = "/gfa")
   public String linkToGfaPages() {
     return "gfa_pages";
+  }
+
+  @GetMapping(value = "/gfa/list")
+  public String listStudents(Model model) {
+    model.addAttribute("students", studentService.findAll());
+    return "gfa_students";
   }
 }
