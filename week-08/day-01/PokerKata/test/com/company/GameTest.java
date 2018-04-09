34edd810e82af2fd6b887eef3e8038ef09aeec63
balazs-varga <balazsvarga91@gmail.com>
2018-04-09 12:40:12 +0200

    feat(PokerKata): another test case is added

--- week-08/day-01/PokerKata/test/com/company/GameTest.java
+++ week-08/day-01/PokerKata/test/com/company/GameTest.java
@@ -3,17 +3,42 @@ package com.company;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class GameTest {
   Player black = new Player();
   Player white = new Player();
   Game game = new Game();
 
   @Test
-  public void highCardTest() {
-    black.newHand(new Card(2, "H"), new Card(3, "D"), new Card(5, "S"), new Card(9, "C"), new Card("K", "D"));
-    white.newHand(new Card(2, "C"), new Card(3, "H"), new Card(4, "S"), new Card(8, "C"), new Card("A", "H"));
+  public void highCardTest1() {
+    black.newHand(
+            new Card(2, "H"),
+            new Card(3, "D"),
+            new Card(5, "S"),
+            new Card(9, "C"),
+            new Card("K", "D"));
+    white.newHand(
+            new Card(2, "C"),
+            new Card(3, "H"),
+            new Card(4, "S"),
+            new Card(8, "C"),
+            new Card("A", "H"));
     Assert.assertEquals(white, game.highCard(black, white));
+  }
+
+  @Test
+  public void highCardTest2() {
+    black.newHand(
+            new Card(5, "H"),
+            new Card(2, "D"),
+            new Card(6, "S"),
+            new Card("J", "C"),
+            new Card("Q", "D"));
+    white.newHand(
+            new Card(5, "C"),
+            new Card(7, "H"),
+            new Card(5, "S"),
+            new Card(8, "C"),
+            new Card(10, "H"));
+    Assert.assertEquals(black, game.highCard(black, white));
   }
 }
