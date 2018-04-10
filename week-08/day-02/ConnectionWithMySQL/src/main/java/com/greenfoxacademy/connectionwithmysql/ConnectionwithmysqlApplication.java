b7cbcfe34d6efd09642c8d5553b017690e7cdc79
balazs-varga <balazsvarga91@gmail.com>
2018-04-10 14:19:48 +0200

    feat(ConnectionWithMySQL): app connected with mySQL

--- week-08/day-02/ConnectionWithMySQL/src/main/java/com/greenfoxacademy/connectionwithmysql/ConnectionwithmysqlApplication.java
+++ week-08/day-02/ConnectionWithMySQL/src/main/java/com/greenfoxacademy/connectionwithmysql/ConnectionwithmysqlApplication.java
@@ -1,12 +1,25 @@
 package com.greenfoxacademy.connectionwithmysql;
 
+import com.greenfoxacademy.connectionwithmysql.models.Todo;
+import com.greenfoxacademy.connectionwithmysql.repositories.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConnectionwithmysqlApplication {
+public class ConnectionwithmysqlApplication implements CommandLineRunner{
+
+	@Autowired
+	TodoRepository todoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConnectionwithmysqlApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		todoRepository.save(new Todo("Play the guitar"));
+		todoRepository.save(new Todo("Do the housework"));
 	}
 }
