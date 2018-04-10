162c16484cd30618da3d2c361039ee2137026e85
balazs-varga <balazsvarga91@gmail.com>
2018-04-10 11:36:20 +0200

    feat(ListingTodosWithH2): multiple urls added and prints the given string

--- week-08/day-02/ListingTodosWithH2/src/main/java/com/example/greenfoxacademy/listingtodoswithh2/controllers/TodoController.java
+++ week-08/day-02/ListingTodosWithH2/src/main/java/com/example/greenfoxacademy/listingtodoswithh2/controllers/TodoController.java
@@ -1,7 +1,16 @@
 package com.example.greenfoxacademy.listingtodoswithh2.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TodoController {
+
+  @GetMapping(value = {"/todo/", "todo/list"})
+  @ResponseBody
+  public String list(Model model) {
+    return "Thanks For Posting!!!";
+  }
 }
