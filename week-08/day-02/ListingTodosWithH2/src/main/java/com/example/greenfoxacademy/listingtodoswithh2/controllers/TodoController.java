9be474c065260a29f56582e9fffa7f54f38c2ac4
balazs-varga <balazsvarga91@gmail.com>
2018-04-10 11:44:10 +0200

    feat(ListingTodosWithH2): created todolist.html

--- week-08/day-02/ListingTodosWithH2/src/main/java/com/example/greenfoxacademy/listingtodoswithh2/controllers/TodoController.java
+++ week-08/day-02/ListingTodosWithH2/src/main/java/com/example/greenfoxacademy/listingtodoswithh2/controllers/TodoController.java
@@ -1,16 +1,19 @@
 package com.example.greenfoxacademy.listingtodoswithh2.controllers;
 
+import com.example.greenfoxacademy.listingtodoswithh2.repositories.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TodoController {
 
+  @Autowired
+  TodoRepository todoRepository;
+
   @GetMapping(value = {"/todo/", "todo/list"})
-  @ResponseBody
   public String list(Model model) {
-    return "Thanks For Posting!!!";
+    return "todoslist";
   }
 }
