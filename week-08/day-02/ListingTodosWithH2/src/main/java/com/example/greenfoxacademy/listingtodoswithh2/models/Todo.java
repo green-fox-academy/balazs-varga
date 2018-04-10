da4177dd505d3499e3e1d63918ae7c20d6a817d4
balazs-varga <balazsvarga91@gmail.com>
2018-04-10 11:55:16 +0200

    feat(ListingTodosWithH2): template shows the todolist

--- week-08/day-02/ListingTodosWithH2/src/main/java/com/example/greenfoxacademy/listingtodoswithh2/models/Todo.java
+++ week-08/day-02/ListingTodosWithH2/src/main/java/com/example/greenfoxacademy/listingtodoswithh2/models/Todo.java
@@ -1,7 +1,15 @@
 package com.example.greenfoxacademy.listingtodoswithh2.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Todo {
 
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Id
   private long id;
   private String title;
   private boolean urgent;
