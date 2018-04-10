70a9aec73010eeca1a6ed7c1c55e55e7b44eb08f
balazs-varga <balazsvarga91@gmail.com>
2018-04-10 11:11:46 +0200

    feat(ListingTodosWithH2): added fields and constructor to the Todo class

--- week-08/day-02/ListingTodosWithH2/src/main/java/com/example/greenfoxacademy/listingtodoswithh2/models/Todo.java
+++ week-08/day-02/ListingTodosWithH2/src/main/java/com/example/greenfoxacademy/listingtodoswithh2/models/Todo.java
@@ -1,4 +1,15 @@
 package com.example.greenfoxacademy.listingtodoswithh2.models;
 
 public class Todo {
+
+  private long id;
+  private String title;
+  private boolean urgent;
+  private boolean done;
+
+  public Todo(String title) {
+    this.title = title;
+    this.urgent = false;
+    this.done = false;
+  }
 }
