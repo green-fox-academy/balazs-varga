a0addef74b3e1926314a300f42522694381c90cd
balazs-varga <balazsvarga91@gmail.com>
2018-04-14 15:11:32 +0200

    feat(Reddit): created fields, constructors, setters and getters in Post class

--- week-08/day-05/Reddit/src/main/java/com/example/greenfoxacademy/reddit/models/Post.java
+++ week-08/day-05/Reddit/src/main/java/com/example/greenfoxacademy/reddit/models/Post.java
@@ -1,4 +1,59 @@
 package com.example.greenfoxacademy.reddit.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Post {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+
+  private int voteNumber;
+  private String url;
+  private String title;
+
+  public Post(String url, String title) {
+    this.url = url;
+    this.title = title;
+    voteNumber = 0;
+  }
+
+  public Post() {
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public int getVoteNumber() {
+    return voteNumber;
+  }
+
+  public void setVoteNumber(int voteNumber) {
+    this.voteNumber = voteNumber;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
 }
