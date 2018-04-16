dd0f91ebdac99aecd02c145eb202da81d57b3ff9
balazs-varga <balazsvarga91@gmail.com>
2018-04-16 12:33:20 +0200

    feat(RestBackend): created showIndexPage method

--- week-09/day-01/Rest_Backend_practice/src/main/java/com/greenfoxacademy/restbackend/controllers/MainController.java
+++ week-09/day-01/Rest_Backend_practice/src/main/java/com/greenfoxacademy/restbackend/controllers/MainController.java
@@ -1,7 +1,13 @@
 package com.greenfoxacademy.restbackend.controllers;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Component
 public class MainController {
+
+  @GetMapping(value = {"/"})
+  public String showIndexPage() {
+    return "index";
+  }
 }
