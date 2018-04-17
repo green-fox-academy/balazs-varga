1b9db81fd16262a0a6939c0f9ae94a0926e517f3
balazs-varga <balazsvarga91@gmail.com>
2018-04-17 11:51:32 +0200

    feat(RestPractice): isDoubling10 test case is added

--- week-09/day-01/Rest_Backend_practice/src/test/java/com/greenfoxacademy/restbackend/RestbackendApplicationTests.java
+++ week-09/day-01/Rest_Backend_practice/src/test/java/com/greenfoxacademy/restbackend/RestbackendApplicationTests.java
@@ -2,15 +2,31 @@ package com.greenfoxacademy.restbackend;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class RestbackendApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Autowired
+  private MockMvc mockMvc;
 
+  @Test
+  public void isDoubling10() throws Exception {
+    mockMvc
+            .perform((get("/doubling/?input=5")))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.received").value(5))
+            .andExpect(jsonPath("$.result").value(10));
+
+  }
 }
---
+++ week-09/day-01/Rest_Backend_practice/src/test/java/com/greenfoxacademy/restbackend/controllers/MainRestControllerTest.java
@@ -0,0 +1,7 @@
+package com.greenfoxacademy.restbackend.controllers;
+
+import static org.junit.Assert.*;
+
+public class MainRestControllerTest {
+
+}
