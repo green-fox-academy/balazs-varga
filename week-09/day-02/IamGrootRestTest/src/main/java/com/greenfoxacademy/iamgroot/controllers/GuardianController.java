5ca1d3eedce44ee4b354e32723af2c879de46fed
balazs-varga <balazsvarga91@gmail.com>
2018-04-17 13:01:15 +0200

    feat(IamGroot): grootTranslator method is created

--- week-09/day-02/IamGrootRestTest/src/main/java/com/greenfoxacademy/iamgroot/controllers/GuardianController.java
+++ week-09/day-02/IamGrootRestTest/src/main/java/com/greenfoxacademy/iamgroot/controllers/GuardianController.java
@@ -1,7 +1,20 @@
 package com.greenfoxacademy.iamgroot.controllers;
 
+import com.greenfoxacademy.iamgroot.models.JsonError;
+import com.greenfoxacademy.iamgroot.models.JsonResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GuardianController {
+
+  @GetMapping("/groot")
+  public Object grootTranslator(@RequestParam(name = "message", required = false) String message) {
+    if (message != null) {
+      return new JsonResponse(message);
+    } else {
+      return new JsonError();
+    }
+  }
 }
