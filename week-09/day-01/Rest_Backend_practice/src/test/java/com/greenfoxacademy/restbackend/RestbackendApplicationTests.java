bac31d1a64e257356d22fc429383eb7a13dbda38
balazs-varga <balazsvarga91@gmail.com>
2018-04-17 11:57:06 +0200

    feat(RestPractice): isGreeterPetikeStudentTest test case is added

--- week-09/day-01/Rest_Backend_practice/src/test/java/com/greenfoxacademy/restbackend/RestbackendApplicationTests.java
+++ week-09/day-01/Rest_Backend_practice/src/test/java/com/greenfoxacademy/restbackend/RestbackendApplicationTests.java
@@ -1,24 +1,42 @@
 package com.greenfoxacademy.restbackend;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.Charset;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class RestbackendApplicationTests {
 
+  private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+          MediaType.APPLICATION_JSON.getSubtype(),
+          Charset.forName("utf8"));
+
   @Autowired
   private MockMvc mockMvc;
+
+  @Autowired
+  private WebApplicationContext webApplicationContext;
+
+  @Before
+  public void setup() throws Exception {
+    this.mockMvc = webAppContextSetup(webApplicationContext).build();
+  }
 
   @Test
   public void isDoubling10() throws Exception {
@@ -37,5 +55,13 @@ public class RestbackendApplicationTests {
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.error").value("Please provide an input!"));
 
+  }
+
+  @Test
+  public void isGreeterPetikeStudentTest() throws Exception {
+    mockMvc
+            .perform((get("/greeter/?name=Petike&title=student")))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.welcome_message").value("Oh, hi there Petike, my dear student!"));
   }
 }
