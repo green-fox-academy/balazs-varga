0f676886b5b439f311d53628d65995f31e2359d8
balazs-varga <balazsvarga91@gmail.com>
2018-04-24 09:53:24 +0200

    feat(TodoApi): created project

---
+++ week-10/TodoAPI/src/main/java/com/greenfoxacademy/todoapi/TodoapiApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.todoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoapiApplication.class, args);
+	}
+}
