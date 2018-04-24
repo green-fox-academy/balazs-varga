c117145a81e2b6d5da05cd50afe7040745afb584
balazs-varga <balazsvarga91@gmail.com>
2018-04-24 10:23:22 +0200

    feat(TodoApi): created Todo class

---
+++ week-10/TodoAPI/src/main/java/com/greenfoxacademy/todoapi/modells/Todo.java
@@ -0,0 +1,55 @@
+package com.greenfoxacademy.todoapi.modells;
+
+import javax.persistence.*;
+
+@Entity
+public class Todo {
+
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Id
+  private long id;
+  private String title;
+  private boolean urgent;
+  private boolean done;
+
+  public Todo(String title) {
+    this.title = title;
+    this.urgent = false;
+    this.done = false;
+  }
+
+  public Todo() {
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public boolean isUrgent() {
+    return urgent;
+  }
+
+  public void setUrgent(boolean urgent) {
+    this.urgent = urgent;
+  }
+
+  public boolean isDone() {
+    return done;
+  }
+
+  public void setDone(boolean done) {
+    this.done = done;
+  }
+}
