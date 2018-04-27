3faf28057dcd73f9fe07b3eb6807add49d5ae91a
balazs-varga <balazsvarga91@gmail.com>
2018-04-27 15:56:08 +0200

    refact(TodoApi): deleted unnecessary lines

--- week-10/TodoAPI/src/main/java/com/greenfoxacademy/todoapi/config/SecurityConfig.java
+++ week-10/TodoAPI/src/main/java/com/greenfoxacademy/todoapi/config/SecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
@@ -37,13 +36,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .addFilter(new JWTAuthorizationFilter(authenticationManager(), customUserDetailsService));
   }
 
-//  @Bean
-//  public static NoOpPasswordEncoder passwordEncoder() {
-//    return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
-//  }
-
   @Bean
   public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder(16);
+    return new BCryptPasswordEncoder();
   }
 }
