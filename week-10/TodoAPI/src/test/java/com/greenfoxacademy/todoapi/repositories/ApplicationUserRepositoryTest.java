886b784a70e37276f21dfaf2362a0cd3d22a8892
balazs-varga <balazsvarga91@gmail.com>
2018-04-27 11:24:38 +0200

    feat(TodoApi): created a test case that uses H2 in memory database to test the findAll method of the ApplicationUserRepository class

--- week-10/TodoAPI/src/test/java/com/greenfoxacademy/todoapi/repositories/ApplicationUserRepositoryTest.java
+++ week-10/TodoAPI/src/test/java/com/greenfoxacademy/todoapi/repositories/ApplicationUserRepositoryTest.java
@@ -2,6 +2,7 @@ package com.greenfoxacademy.todoapi.repositories;
 
 import com.greenfoxacademy.todoapi.models.ApplicationUser;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
@@ -10,6 +11,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
@@ -38,5 +42,23 @@ public class ApplicationUserRepositoryTest {
     thirdUser = new ApplicationUser();
     thirdUser.setUsername("guest");
     thirdUser.setPassword("kfaskkna");
+  }
+
+  @Test
+  public void findAllTest() throws Exception {
+    //given
+    entityManager.persist(firstUser);
+    entityManager.persist(secondUser);
+    entityManager.persist(thirdUser);
+    entityManager.flush();
+
+    //when
+    List<ApplicationUser> userList = applicationUserRepository.findAll();
+
+    //then
+    assertThat(userList.size()).isEqualTo(3);
+    assertThat(userList.get(0)).isEqualTo(firstUser);
+    assertThat(userList.get(1)).isEqualTo(secondUser);
+    assertThat(userList.get(2)).isEqualTo(thirdUser);
   }
 }
